de53c893c7dd131292c23f96581b968b83767308
ebner <simon.ebner@psi.ch>
2017-04-25 14:12:12 +0200

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="data_api",
-    version="0.1.9",
+    version="0.2.0",
     author="Leonardo Sala",
     author_email="leonardo.sala@psi.ch",
     description=("Pandas interface to PSI's Data API"),
