dcdb243820ed75bf5e26af4dc50ee1d8a062514b
Simon Ebner <simon.ebner@psi.ch>
2018-04-18 10:50:13 +0200

    fixed version in setup.py/metadata.yaml removed compression

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="data_api",
-    version="0.5.0",
+    version="0.6.0",
     author="Paul Scherrer Institute",
     author_email="daq@psi.ch",
     description=("Interface to PSI's Data API"),
